cedf08355a76f08ccde6fb2fee224d74d1914aa1
dyutidutta <159049745+dyutidutta@users.noreply.github.com>
2025-08-14 21:44:37 +0530

    Update magic_square.cpp

--- Magic Square/magic_square.cpp
+++ Magic Square/magic_square.cpp
@@ -2,96 +2,65 @@
 #include <vector>
 using namespace std;
 
-void oddMagicSquare(int n, vector<vector<int>> &magic)
+void oddMagicSquare(int n, vector<vector<int>> &m)
 {
     int i = 0, j = n / 2;
     for (int num = 1; num <= n * n; num++)
     {
-        magic[i][j] = num;
-        int newi = (i - 1 + n) % n;
-        int newj = (j + 1) % n;
-        if (magic[newi][newj] != 0)
-        {
+        m[i][j] = num;
+        int ni = (i - 1 + n) % n, nj = (j + 1) % n;
+        if (m[ni][nj])
             i = (i + 1) % n;
-        }
         else
-        {
-            i = newi;
-            j = newj;
-        }
+            i = ni, j = nj;
     }
 }
 
-void doublyEvenMagicSquare(int n, vector<vector<int>> &square)
+void doublyEvenMagicSquare(int n, vector<vector<int>> &s)
 {
     int num = 1;
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
-            square[i][j] = ((i % 4 == j % 4) || ((i % 4 + j % 4) == 3))
-                               ? n * n + 1 - num++
-                               : num++;
-        }
-    }
+            s[i][j] = ((i % 4 == j % 4) || ((i % 4 + j % 4) == 3))
+                          ? n * n + 1 - num++
+                          : num++;
 }
 
-void singlyEvenMagicSquare(int n, vector<vector<int>> &square)
+void singlyEvenMagicSquare(int n, vector<vector<int>> &s)
 {
-    int half = n / 2;
-    int subSize = half * half;
-    vector<vector<int>> sub(half, vector<int>(half, 0));
+    int h = n / 2, subSize = h * h;
+    vector<vector<int>> sub(h, vector<int>(h));
+    oddMagicSquare(h, sub);
 
-    int i = 0, j = half / 2;
-    for (int num = 1; num <= subSize; num++)
-    {
-        sub[i][j] = num;
-        int ni = (i - 1 + half) % half;
-        int nj = (j + 1) % half;
-        if (sub[ni][nj] != 0)
-            i = (i + 1) % half;
-        else
+    for (int r = 0; r < h; r++)
+        for (int c = 0; c < h; c++)
         {
-            i = ni;
-            j = nj;
+            int v = sub[r][c];
+            s[r][c] = v;
+            s[r + h][c + h] = v + subSize;
+            s[r][c + h] = v + 2 * subSize;
+            s[r + h][c] = v + 3 * subSize;
         }
-    }
 
-    for (int r = 0; r < half; r++)
-    {
-        for (int c = 0; c < half; c++)
-        {
-            int val = sub[r][c];
-            square[r][c] = val;
-            square[r + half][c + half] = val + subSize;
-            square[r][c + half] = val + 2 * subSize;
-            square[r + half][c] = val + 3 * subSize;
-        }
-    }
-
-    
-    int k = (half - 1) / 2;
-    for (int r = 0; r < half; r++)
+    int k = (h - 1) / 2;
+    for (int r = 0; r < h; r++)
         for (int c = 0; c < k; c++)
-            swap(square[r][c], square[r + half][c]);
-
-    for (int r = 0; r < half; r++)
+            swap(s[r][c], s[r + h][c]);
+    for (int r = 0; r < h; r++)
         for (int c = n - k + 1; c < n; c++)
-            swap(square[r][c], square[r + half][c]);
+            swap(s[r][c], s[r + h][c]);
 }
 
-
-void printMagicSquare(const vector<vector<int>> &square)
+void printMagicSquare(const vector<vector<int>> &s)
 {
-    int n = square.size();
-    for (auto &row : square)
+    int n = s.size();
+    for (auto &row : s)
     {
-        for (int val : row)
-            cout << val << "\t";
+        for (int v : row)
+            cout << v << "\t";
         cout << "\n";
     }
-    int magicConstant = n * (n * n + 1) / 2;
-    cout << "\nMagic Constant = " << magicConstant << "\n";
+    cout << "\nMagic Constant = " << n * (n * n + 1) / 2 << "\n";
 }
 
 int main()
@@ -99,30 +68,18 @@ int main()
     int n;
     cout << "Enter size of Magic Square (n >= 3): ";
     cin >> n;
-
     if (n < 3)
-    {
-        cout << "Magic square not possible for n < 3.\n";
-        return 0;
-    }
+        return cout << "Magic square not possible for n < 3.\n", 0;
 
-    vector<vector<int>> square(n, vector<int>(n, 0));
-
-    if (n % 2 == 1)
-    {
+    vector<vector<int>> square(n, vector<int>(n));
+    if (n % 2)
         oddMagicSquare(n, square);
-    }
     else if (n % 4 == 0)
-    {
         doublyEvenMagicSquare(n, square);
-    }
     else
-    {
         singlyEvenMagicSquare(n, square);
-    }
 
     cout << "\nMagic Square of size " << n << ":\n";
     printMagicSquare(square);
-
     return 0;
 }
